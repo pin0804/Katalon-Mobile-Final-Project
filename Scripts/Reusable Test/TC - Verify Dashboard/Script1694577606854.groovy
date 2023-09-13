5133c71eb73dbd16a7217dbd6ac6f31d4d2e7a1f
agfina <agfinaa@gmail.com>
2023-09-13 11:01:31 +0700

    add tc for verify element dashboard after made accounts

---
+++ Scripts/Reusable Test/TC - Verify Dashboard/Script1694577606854.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.verifyElementVisible(findTestObject('Login Page/Dashboard with no acc/txt- Dashboard'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Login Page/Dashboard with no acc/btn - menu burger'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Login Page/btn - help'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Login Page/btn - menu kebab'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Login Page/Dashboard with no acc/img - logo'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Login Page/Dashboard with no acc/txt - Welcome to the Bank App Demo'), 0)
+
